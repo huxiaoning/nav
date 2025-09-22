bfd376e9b65c1bcbcef459e7f1a996f75fd2f916
huxiaoning <huxiaoning@users.noreply.github.com>
2025-09-22 04:29:15 +0000

    Deploying to gh-pages from @ huxiaoning/nav@40aa4a5094480b622a792586cacbcdc2524040de 🚀

---
+++ chunk-3SWDFJT2.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-3W5JQIA5.js";import{qb as r,zb as e}from"./chunk-XK2RVGQV.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
